dc235a396b89c72d2780fde49dd81da57eb54537
namdev1994 <n.lavhare1994@gmail.com>
2021-04-27 01:49:53 +0530

    Added: Calculate Length Of Line

--- src/lineComparision/LineComparision.java
+++ src/lineComparision/LineComparision.java
@@ -4,6 +4,15 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		double x1=2,y1=1,x2=4,y2=5;
+		double x=x2-x1;
+		double y=y2-y1;
+		x=x*x;
+		y=y*y;
+		double z=x+y;
+		double a=Math.sqrt(z);
+		System.out.println(a);
+		
 
 	}
 
