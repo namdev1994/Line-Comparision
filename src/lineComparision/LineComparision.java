0b1f4380a9b3e5092f3da306b33b90515b3328d1
namdev1994 <n.lavhare1994@gmail.com>
2021-05-01 01:27:42 +0530

    Added :Line comparision using OOP Concept

--- src/lineComparision/LineComparision.java
+++ src/lineComparision/LineComparision.java
@@ -3,15 +3,21 @@ package lineComparision;
 import java.util.Scanner;
 
 public class LineComparision {
-
+	static Double length1=0d;
+	static Double length2=0d;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		//method call
-		lengthCalculation();
+		//object Creation
+		LineComparision l1=new LineComparision();
+		//method call using object
+		l1.lengthCalculation();
+		//equal method call using object
+		l1.UsingEqualsMethod();
+		//CompareTo method call using object
+		l1.UsingCopareToMethod();
 	}
-	
-	
-	//method Implementation
+		
+		//length Calculation
 		public static void lengthCalculation()
 		{
 			double x1,x2,x3,x4,y1,y2,y3,y4;
@@ -21,15 +27,19 @@ public class LineComparision {
 			x2=sc.nextDouble();
 			y1=sc.nextDouble();
 			y2=sc.nextDouble();
-			Double length1=Math.sqrt((x2 - x1)*(x2 - x1) + (y2 -y1)*(y2 -y1));
+			length1=Math.sqrt((x2 - x1)*(x2 - x1) + (y2 -y1)*(y2 -y1));
 			System.out.println("Length of Line 1 = "+length1);
 			System.out.println("Enter the  Co-ordinates of 2nd Line");
 			x3=sc.nextDouble();
 			x4=sc.nextDouble();
 			y3=sc.nextDouble();
 			y4=sc.nextDouble();
-			Double length2=Math.sqrt((x4 - x3)*(x4 - x3) + (y4 -y3)*(y4 -y3));
+			 length2=Math.sqrt((x4 - x3)*(x4 - x3) + (y4 -y3)*(y4 -y3));
 			System.out.println("Length of Line 2 = "+length2);
+			
+		}
+		public static void UsingEqualsMethod()
+		{
 			if(length1.equals(length2))
 			{
 				System.out.println("The length of line are equals");
@@ -38,6 +48,9 @@ public class LineComparision {
 			{
 				System.out.println("The length of line are different");
 			}
+		}
+		public static void UsingCopareToMethod()
+		{
 			int check=length1.compareTo(length2);
 			if(check == 0)
 			{
@@ -51,6 +64,7 @@ public class LineComparision {
 			{
 				System.out.println("The length of line 2 is greater than Frist");
 			}
+			
 		}
 		
 		
